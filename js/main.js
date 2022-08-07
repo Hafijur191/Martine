4976e877187f6c3e5dc76b11371f04d48f6da2f6
Md Hafijur Rahoman <107298626+Hafijur191@users.noreply.github.com>
2022-08-08 01:48:20 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
 $(document).ready(function () {
   $(".hotel-item").show();
   $(".tricket-item").hide();
-   $(".place-item").hide();
-    
-    $("#hotel span").addClass('line');
+  $(".place-item").hide();
+
+  $("#hotel span").addClass("line");
 
   // for hotel
 
   $("#hotel").click(function () {
     $(".hotel-item").show();
     $(".tricket-item").hide();
-      $(".place-item").hide();
-      
+    $(".place-item").hide();
+
     $("#hotel span").addClass("line");
     $("#tricket span").removeClass("line");
     $("#place span").removeClass("line");
-
   });
 
   // for tricket
@@ -23,11 +22,11 @@ $(document).ready(function () {
   $("#tricket").click(function () {
     $(".hotel-item").hide();
     $(".tricket-item").show();
-      $(".place-item").hide();
-      
-      $("#hotel span").removeClass("line");
-      $("#tricket span").addClass("line");
-      $("#place span").removeClass("line");
+    $(".place-item").hide();
+
+    $("#hotel span").removeClass("line");
+    $("#tricket span").addClass("line");
+    $("#place span").removeClass("line");
   });
 
   // for place
@@ -35,10 +34,10 @@ $(document).ready(function () {
   $("#place").click(function () {
     $(".hotel-item").hide();
     $(".tricket-item").hide();
-      $(".place-item").show();
-      
-      $("#hotel span").removeClass("line");
-      $("#tricket span").removeClass("line");
-      $("#place span").addClass("line");
+    $(".place-item").show();
+
+    $("#hotel span").removeClass("line");
+    $("#tricket span").removeClass("line");
+    $("#place span").addClass("line");
   });
 });
